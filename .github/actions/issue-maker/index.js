6eae3eda4ed99ae9d7f1e8f8fb9d59990878e34e
Chris Breen <chris@kurl.co.uk>
2020-03-27 20:05:51 +0000

    create index.js

---
+++ .github/actions/issue-maker/index.js
@@ -0,0 +1,23 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+const run = () => {
+  return new Promise((resolve, reject) => {
+    const issueTitle = core.getInput("issue-title");
+    const jokeBody = core.getInput("joke");
+    const token = code.getInput("repo-token");
+
+    const octokit = new github.GitHub(token);
+
+    return octokit.issues.create({
+      repo: github.context.repo.repo,
+      owner: github.context.repo.owner,
+      title: issueTitle,
+      body: jokeBody
+    });
+  });
+};
+
+run()
+  .then(console.log)
+  .catch(err => core.setFailed(err.message));
